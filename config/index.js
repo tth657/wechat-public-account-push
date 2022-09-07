fa3440eb0cf3c82982bfe1b240d15e7028251993
tth657 <113002750+tth657@users.noreply.github.com>
2022-09-07 13:12:35 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -74,7 +74,7 @@ export const config = {
    */
 
   // 回调消息模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: "zjV1glIuCXo4Csr23aXWEio1iP74sZJ7IQEES95lDsg6c4yOU",
+  CALLBACK_TEMPLATE_ID: "573ihgmVLfU32c2dd3ugvWmPZudVRraSccgbv4IgIFU",
 
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [
@@ -106,10 +106,10 @@ export const config = {
    * 节日时，name填写相应展示的节日，你可以填“被搭讪纪念日”
    */
   FESTIVALS: [
-    {"type": "生日", "name": "老婆", "year": "1996", "date": "09-02"},
-    {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
-    {"type": "生日", "name": "李四", "year": "1996", "date": "09-31"},
-    {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
+    {"type": "生日", "name": "乖乖宝", "year": "2002", "date": "05-07"},
+    {"type": "节日", "name": "在一起纪念日", "year": "2020", "date": "07-30"},
+    {"type": "生日", "name": "欣欣宝贝", "year": "2002", "date": "06-05"},
+    {"type": "节日", "name": "被搭讪纪念日", "year": "2023", "date": "07-30"},
   ],
 
   /**
@@ -127,9 +127,7 @@ export const config = {
     * */
   CUSTOMIZED_DATE_LIST: [
     // 在一起的日子
-    {"keyword": "love_day", date: "2015-05-01"},
-    // 结婚纪念日
-    {"keyword": "marry_day", date: "2020-01-04"},
+    {"keyword": "love_day", date: "2020-07-30"},
     // 退伍日, 不用可以删掉
     {"keyword": "ex_day", date: "2022-08-31"},
     // sakana日
